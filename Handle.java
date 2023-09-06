8507477259095a1cbb8351eb117c2fb3739d52e5
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-06 15:29:11 -0400

    Update Handle.java

--- Handle.java
+++ Handle.java
@@ -1,11 +1,26 @@
-public class Handle 
-{
+/**
+ * The Handle class represents a unique identifier (handle) for records.
+ * It provides a simple way to retrieve and manage the ID of records.
+ */
+public class Handle {
+    
+    // Instance variable to store the unique identifier
     private int id;
 
+    /**
+     * Constructor to initialize a new Handle object with a given ID.
+     *
+     * @param id Unique identifier for the handle.
+     */
     public Handle(int id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the unique identifier (ID) of the handle.
+     * 
+     * @return The ID of the handle.
+     */
     public int getID() {
         return id;
     }
