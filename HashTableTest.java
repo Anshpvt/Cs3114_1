9f2156260d85274f369d741396476d48d79ca926
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-07 17:43:46 -0400

    Update HashTableTest.java

--- HashTableTest.java
+++ HashTableTest.java
@@ -1,95 +1,66 @@
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
-public class SeminarDBTest {
+public class HashTableTest {
 
-    private Record testRecord;
     private HashTable hashTable;
 
-    public void setup() {
-        testRecord = new Record(1, "Test Title", "10:00", "1hr", "5", "6", "$100", "test,example", "Test description.");
-        hashTable = new HashTable();  // Instantiate a new HashTable for each test
+    public void setUp() {
+        hashTable = new HashTable();
     }
 
-    public void testInsert() {
-        boolean wasInserted = HashTable.insert(
-            testRecord.getHandle().getID(),
-            testRecord.getTitle(),
-            testRecord.getTime(),
-            testRecord.getLength(),
-            testRecord.getX(),
-            testRecord.getY(),
-            testRecord.getCost(),
-            testRecord.getList(),
-            testRecord.getDescription()
-        );
 
-        assertTrue(wasInserted);
-        assertNotNull(HashTable.search(testRecord.getHandle().getID()));
+    public void testInsert() {
+        // Test insertion of a new record
+        assertTrue(hashTable.insert(1, "title1", "10:00", "2 hours", "1.2",
+            "2.3", "50", "list1", "description1"));
+
+        // Test that inserting a duplicate ID fails
+        assertFalse(hashTable.insert(1, "title2", "11:00", "3 hours", "2.3",
+            "3.4", "60", "list2", "description2"));
     }
 
     public void testDelete() {
-        HashTable.insert(
-            testRecord.getHandle().getID(),
-            testRecord.getTitle(),
-            testRecord.getTime(),
-            testRecord.getLength(),
-            testRecord.getX(),
-            testRecord.getY(),
-            testRecord.getCost(),
-            testRecord.getList(),
-            testRecord.getDescription()
-        );
+        // Set up for delete
+        hashTable.insert(2, "title2", "11:00", "3 hours", "2.3", "3.4", "60",
+            "list2", "description2");
 
-        boolean wasDeleted = HashTable.delete(testRecord.getHandle().getID());
+        // Test that deleting an existing record works
+        assertTrue(hashTable.delete(2));
 
-        assertTrue(wasDeleted);
-        assertNull(HashTable.search(testRecord.getHandle().getID()));
+        // Test that deleting a non-existent record fails
+        assertFalse(hashTable.delete(2));
     }
 
-    public void testSearchFound() {
-        HashTable.insert(
-            testRecord.getHandle().getID(),
-            testRecord.getTitle(),
-            testRecord.getTime(),
-            testRecord.getLength(),
-            testRecord.getX(),
-            testRecord.getY(),
-            testRecord.getCost(),
-            testRecord.getList(),
-            testRecord.getDescription()
-        );
 
-        String result = HashTable.search(testRecord.getHandle().getID());
+    public void testSearch() {
+        // Set up for search
+        hashTable.insert(3, "title3", "12:00", "4 hours", "3.4", "4.5", "70",
+            "list3", "description3");
 
-        assertNotNull(result);
-        assertTrue(result.contains(testRecord.getTitle()));
+        // Test that searching for an existing record returns the correct record
+        String expected =
+            "ID: 3, Title: title3, Time: 12:00, Length: 4 hours, X: 3.4, Y: 4.5, Cost: 70, List: list3, Description: description3";
+        assertEquals(expected, hashTable.search(3));
+
+        // Test that searching for a non-existent record returns null
+        assertNull(hashTable.search(4));
     }
 
-    public void testSearchNotFound() {
-        String result = HashTable.search(999);  // Assuming the record with ID 999 does not exist
 
-        assertNull(result);
-    }
+    public void testPrint() {
+        // Set up for print
+        hashTable.insert(4, "title4", "01:00", "5 hours", "4.5", "5.6", "80",
+            "list4", "description4");
+        hashTable.insert(5, "title5", "02:00", "6 hours", "5.6", "6.7", "90",
+            "list5", "description5");
 
-    public void testPrintHashTable() {
-        // Insert the test record
-        HashTable.insert(
-            testRecord.getHandle().getID(),
-            testRecord.getTitle(),
-            testRecord.getTime(),
-            testRecord.getLength(),
-            testRecord.getX(),
-            testRecord.getY(),
-            testRecord.getCost(),
-            testRecord.getList(),
-            testRecord.getDescription()
-        );
+        String expected =
+            "ID: 4, Title: title4, Time: 01:00, Length: 5 hours, X: 4.5, Y: 5.6, Cost: 80, List: list4, Description: description4\n"
+                + "ID: 5, Title: title5, Time: 02:00, Length: 6 hours, X: 5.6, Y: 6.7, Cost: 90, List: list5, Description: description5\n";
 
-        // Call the print method for hashtable
-        String result = HashTable.print();
-        
-        // Check if the result string contains our inserted record's details
-        assertTrue(result.contains(testRecord.toString()));
+        assertEquals(expected, hashTable.print());
     }
 }
 
