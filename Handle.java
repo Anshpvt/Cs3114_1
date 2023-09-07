dccf5776ac6029044beefcdae00d73633c280c59
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-07 00:03:21 -0400

    Update Handle.java

--- Handle.java
+++ Handle.java
@@ -3,18 +3,20 @@
  * It provides a simple way to retrieve and manage the ID of records.
  */
 public class Handle {
-    
+
     // Instance variable to store the unique identifier
     private int id;
 
     /**
      * Constructor to initialize a new Handle object with a given ID.
      *
-     * @param id Unique identifier for the handle.
+     * @param id
+     * Unique identifier for the handle.
      */
     public Handle(int id) {
         this.id = id;
     }
+
 
     /**
      * Retrieves the unique identifier (ID) of the handle.
