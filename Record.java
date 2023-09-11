1e5c88faec030823657af051ad4845ae130a31d8
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-11 13:40:03 -0400

    Update Record.java

--- Record.java
+++ Record.java
@@ -1,113 +1,55 @@
 /**
- * The Record class represents a single record entry with various attributes.
- * Each record has an ID, title, time, length, coordinates (x, y), cost, a list
- * of keywords, and a description.
+ * Represents a single record entry in a database or data structure.
+ * This record includes various attributes such as ID, title, time,
+ * length, coordinates, cost, a list of keywords, and a description.
+ * For simplicity, this version uses an integer key and a general object for value, 
+ * but in a more comprehensive version, those attributes mentioned would be individually represented.
  */
 public class Record {
 
-    // Instance variables for the various attributes of the record
-    private int id;
-    private String title, time, length, x, y, cost, list, des;
+    // The unique identifier for this record. Could represent the ID.
+    private int key;
+
+    // Represents the data associated with this record. Could be any type of object
+    // encapsulating attributes like title, time, length, coordinates, cost, keywords, etc.
+    private Object value;
 
     /**
-     * Constructor to initialize a new Record object with given parameters.
-     *
-     * @param id
-     *            Unique identifier for the record.
-     * @param title
-     *            Title of the record.
-     * @param time
-     *            Time associated with the record.
-     * @param length
-     *            Duration or length of the record.
-     * @param x
-     *            X-coordinate or other relevant data.
-     * @param y
-     *            Y-coordinate or other relevant data.
-     * @param cost
-     *            Cost associated with the record.
-     * @param list
-     *            A list of keywords or other data.
-     * @param des
-     *            Description of the record.
+     * Constructor to initialize a record with a given key and associated value.
+     * 
+     * @param key   The unique identifier for the record.
+     * @param value The associated data of the record.
      */
-    public Record(
-        int id,
-        String title,
-        String time,
-        String length,
-        String x,
-        String y,
-        String cost,
-        String list,
-        String des) {
-    	
-        this.id = id;
-        this.title = title;
-        this.time = time;
-        this.length = length;
-        this.x = x;
-        this.y = y;
-        this.cost = cost;
-        this.list = list;
-        this.des = des;
-    }
-    
-    public int getId()
-    {
-    	return id;
-    }
-    
-    public String getTitle() {
-        return title;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public String getLength() {
-        return length;
-    }
-
-    public String getX() {
-        return x;
-    }
-
-    public String getY() {
-        return y;
-    }
-
-    public String getCost() {
-        return cost;
-    }
-
-    public String getList() {
-        return list;
-    }
-
-    public String getDescription() {
-        return des;
+    public Record(int key, Object value) {
+        this.key = key;
+        this.value = value;
     }
 
     /**
-     * Gets the handle (unique identifier) for this record.
-     *
-     * @return Handle object containing the ID of the record.
+     * Retrieves the key of this record.
+     * 
+     * @return The key of the record.
      */
-    public Handle getHandle() {
-        return new Handle(id);
+    public int getKey() {
+        return key;
     }
 
+    /**
+     * Retrieves the associated data of this record.
+     * 
+     * @return The value of the record.
+     */
+    public Object getValue() {
+        return value;
+    }
 
     /**
      * Provides a string representation of the record.
      * 
-     * @return A string detailing all attributes of the record.
+     * @return A string showing the key and value of the record.
      */
+    @Override
     public String toString() {
-        return "ID: " + id + ", Title: " + title + ", Time: " + time
-            + ", Length: " + length + ", X: " + x + ", Y: " + y + ", Cost: "
-            + cost + ", List: " + list + ", Description: " + des;
+        return "Record{" + "key=" + key + ", value=" + value + '}';
     }
 }
