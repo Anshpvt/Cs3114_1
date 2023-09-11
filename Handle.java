b258f8a0c65f21f461bd22f4ad9058cb5f3812b8
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-11 14:20:38 -0400

    Update Handle.java

--- Handle.java
+++ Handle.java
@@ -1,22 +1,37 @@
 /**
  * The Handle class represents a unique identifier (handle) for records.
- * It provides a simple way to retrieve and manage the ID of records.
+ * It not only stores the unique ID but also the start location and length
+ * of the message in the memory manager. This ensures efficient and accurate
+ * access to the data stored in memory.
  */
 public class Handle {
 
-    // Instance variable to store the unique identifier
+    // Unique identifier for the handle.
     private int id;
 
+    // Start location of the message in the memory manager.
+    private int startLocation;
+
+    // Length of the message in the memory manager.
+    private int length;
+
     /**
-     * Constructor to initialize a new Handle object with a given ID.
+     * Constructor to initialize a new Handle object with a given ID, start location, and length.
      *
      * @param id
      * Unique identifier for the handle.
+     * 
+     * @param startLocation
+     * Start location of the message in the memory manager.
+     * 
+     * @param length
+     * Length of the message in the memory manager.
      */
-    public Handle(int id) {
+    public Handle(int id, int startLocation, int length) {
         this.id = id;
+        this.startLocation = startLocation;
+        this.length = length;
     }
-
 
     /**
      * Retrieves the unique identifier (ID) of the handle.
@@ -25,5 +40,23 @@ public class Handle {
      */
     public int getID() {
         return id;
+    }
+
+    /**
+     * Retrieves the start location of the message in the memory manager.
+     * 
+     * @return The start location.
+     */
+    public int getStartLocation() {
+        return startLocation;
+    }
+
+    /**
+     * Retrieves the length of the message in the memory manager.
+     * 
+     * @return The length of the message.
+     */
+    public int getLength() {
+        return length;
     }
 }
