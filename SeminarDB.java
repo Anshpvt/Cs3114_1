b61ee4dac2a3cd4a9e6097c518096b89fe479dd3
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-11 14:21:54 -0400

    Update SeminarDB.java

--- SeminarDB.java
+++ SeminarDB.java
@@ -1,138 +1,46 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * SeminarDB class represents a database for seminars, providing functionalities
- * like insertion,
- * deletion, search, and printing of seminar records using a hash table.
- */
 public class SeminarDB {
-	private HashTable hash;
 
-    /**
-     * Default constructor that initializes the SeminarDB.
-     * If additional initializations are needed, they can be added here.
-     */
-    public SeminarDB(int memSize, int hashSize) {
-        // Possibly initialize the HashTable or other internal structures here.
-    	hash = new HashTable(hashSize);
+    private Map<Integer, Seminar> seminars;
+    private MemoryManager memManager;
+
+    public SeminarDB(int memorySize) {
+        this.seminars = new HashMap<>();
+        this.memManager = new MemoryManager(memorySize);
     }
 
+    public void insert(int id, String title, String date, int length, short x, 
+        short y, int cost, String[] keywords, String desc) {
+        
+        // Check if we have memory to allocate for the seminar
+        int memoryNeeded = title.length() + date.length() + desc.length() + (keywords.length * 10);  // assuming an average of 10 characters per keyword
+        int address = memManager.allocate(memoryNeeded);
 
-    /**
-     * Inserts a seminar record into the database.
-     *
-     * @param id
-     *            ID of the seminar.
-     * @param title
-     *            Title of the seminar.
-     * @param time
-     *            Time when the seminar is scheduled.
-     * @param length
-     *            Duration of the seminar.
-     * @param x
-     *            The X coordinate of the seminar location.
-     * @param y
-     *            The Y coordinate of the seminar location.
-     * @param cost
-     *            The cost to attend the seminar.
-     * @param list
-     *            A list of keywords associated with the seminar.
-     * @param des
-     *            A description of the seminar.
-     */
-    public void insert(
-        int id,
-        String title,
-        String time,
-        String length,
-        String x,
-        String y,
-        String cost,
-        String list,
-        String des) {
-        // Assuming HashTable.insert() returns true if the insertion is
-        // successful.
-    	
-        boolean wasInserted = hash.insert(id, title, time, length, x, y,
-            cost, list, des);// want to change to insert(key, Record)
-
-        if (wasInserted) {
-            System.out.println("Successfully inserted record with ID " + id);
-        }
-        else {
-            // Modify this message if you have a more specific reason for
-            // insertion failure.
-            System.out.println("Insertion failed for record with ID " + id);
+        if (address != -1) {  // we have enough memory
+            Seminar seminar = new Seminar(id, title, date, length, x, y, cost, keywords, desc);
+            seminars.put(id, seminar);
+        } else {
+            System.out.println("Not enough memory to store seminar with ID " + id);
         }
     }
 
-
-    /**
-     * Deletes a seminar record from the database using its ID.
-     *
-     * @param id
-     *            ID of the seminar to be deleted.
-     */
     public void delete(int id) {
-        // Assuming HashTable.delete() returns true if the deletion is
-        // successful.
-        boolean wasDeleted = hash.delete(id);
-
-        if (wasDeleted) {
-            System.out.println("Record with ID " + id
-                + " successfully deleted from the database");
-        }
-        else {
-            System.out.println("Delete FAILED -- There is no record with ID "
-                + id);
+        Seminar seminar = seminars.remove(id);
+        if (seminar != null) {
+            int memoryFreed = seminar.toString().length();
+            memManager.deallocate(memoryFreed, memoryFreed);  // deallocate memory used by this seminar
         }
     }
 
-
-    /**
-     * Searches for a seminar record in the database using its ID.
-     *
-     * @param id
-     *            ID of the seminar to search for.
-     */
-    public void search(int id) {
-        // Assuming HashTable.search() returns the record if found, null
-        // otherwise.
-        String record = hash.search(id);
-
-        if (record != null) {
-            System.out.println("Found record with ID " + id + ": " + record);
-        }
-        else {
-            System.out.println("Record with ID " + id + " not found");
-        }
+    public Seminar search(int id) {
+        return seminars.get(id);
     }
 
-
-    /**
-     * Prints the content of the database. Can print the entire hash table or
-     * the list of free blocks.
-     *
-     * @param type
-     *            A string indicating the type of content to print ("hashtable"
-     *            or "block").
-     */
-    public void print(String type) {
-        // Assuming HashTable.print() returns the entire contents, and there's a
-        // method to print free blocks as well.
-        if ("hashtable".equalsIgnoreCase(type)) {
-            String content = hash.print();
-            System.out.println(content);
-        }
-        else if ("block".equalsIgnoreCase(type)) {
-            // Print the free block list. You can modify this to use a method
-            // you have.
-            System.out.println("Freeblock list");
-        }
-        else {
-            System.out.println("Unrecognized input " + type);
+    public void print() {
+        for (Seminar seminar : seminars.values()) {
+            System.out.println(seminar.toString());
         }
     }
 }
