06f6b0e02201807ba997d226c5ef88ca36860217
stepheny <112502475+dragofsewer@users.noreply.github.com>
2023-09-11 20:35:13 -0400

    Update SeminarDB.java

--- SeminarDB.java
+++ SeminarDB.java
@@ -1,82 +1,106 @@
+
 public class SeminarDB {
 
-    private Node head;
+    private HashTable hash;
     private MemoryManager memManager;
 
-    // Inner class to represent a node in the linked list
-    private class Node {
-        Seminar seminar;
-        Node next;
+    public SeminarDB(int memorySize, int hashSize) {
 
-        Node(Seminar seminar) {
-            this.seminar = seminar;
-            this.next = null;
-        }
-    }
-
-    public SeminarDB(int memorySize) {
-        this.head = null;
         this.memManager = new MemoryManager(memorySize);
+        this.hash = new HashTable(hashSize);
+    }
+    
+    public HashTable getHash()
+    {
+    	return hash;
+    }
+    
+    public boolean isInserted(int key, Record rec)
+    {
+    	return hash.insert(key, rec);
+    }
+    
+    public void insert(Record rec)
+    {
+    	int start = hash.getCapacity();
+    	int key = rec.getKey();
+    	
+    	if (!isInserted(key, rec))
+    	{
+    		System.out.println("Insert FAILED - There is already a record with ID " + key);
+    	}
+    	else
+    	{
+    		if ( start != hash.getCapacity())
+    		{
+    			System.out.print("Hash Table expanded to " + hash.getCapacity() + " records");
+    		}
+    		Seminar sem = rec.getSeminar();
+    		System.out.println("Successfully inserted reecord with ID " + key);
+    		System.out.println(sem.toString());
+    		
+    		byte[] byteArr;
+    		try {
+    			byteArr = sem.serialize();
+    			System.out.println("Size: " + byteArr.length);
+    		}
+    		catch (Exception e) {
+    			e.printStackTrace();
+    		}
+    		
+    	}
+//    	int memoryNeeded = rec.getSem().length() * 10;  // assuming an average of 10 characters per keyword
+//        int address = memManager.allocate(memoryNeeded);
+//        if (address == -1) {  // we dont have enough memory
+//            
+//            System.out.println("Not enough memory to store seminar with ID " + key);
+        //}
+    	
     }
 
-    public void insert(int id, String title, String date, int length, short x, 
-        short y, int cost, String[] keywords, String desc) {
+
+    public boolean isDeleted(int key)
+    {
+    	return hash.delete(key);
+    }
+    
+    public void delete(int key) {
         
-        // Check if we have memory to allocate for the seminar
-        int memoryNeeded = title.length() + date.length() + desc.length() + (keywords.length * 10);  // assuming an average of 10 characters per keyword
-        int address = memManager.allocate(memoryNeeded);
+    	if (!isDeleted(key))
+    	{
+    		System.out.println("Delete FAILED -- There is no record with ID " + key);
+    	}
+    	else
+    	{
+    		System.out.println("Record with ID " + key + " successfully deleted from the database");
+    	}
+    		
+//        if (isDeleted(key)) {
+//            int memoryFreed = getInfo.length();
+//            memManager.deallocate(key, memoryFreed);  // deallocate memory used by this seminar
+//        }
+    }
 
-        if (address != -1) {  // we have enough memory  
-            Seminar seminar = new Seminar(id, title, date, length, x, y, cost, keywords, desc);
-            Node newNode = new Node(seminar);
-            newNode.next = head;
-            head = newNode;
-        } else {
-            System.out.println("Not enough memory to store seminar with ID " + id);
+    public void search(int key) {
+        Record rec = hash.search(key);
+        if (rec == null || rec.isTombstone())
+        {
+        	System.out.println("Search FAILED -- There is no record with ID " + key);
+        }
+        else
+        {
+        	System.out.println("Found record with ID " + key + ": ");
+        	System.out.println(rec.getSeminar().toString());
         }
     }
 
-    public void delete(int id) {
-        if (head == null) return;
-
-        if (head.seminar.id == id) {
-            int memoryFreed = head.seminar.toString().length();
-            memManager.deallocate(memoryFreed, memoryFreed);
-            head = head.next;
-            return;
-        }
-
-        Node current = head;
-        Node prev = null;
-        while (current != null && current.seminar.getId() != id) {
-            prev = current;
-            current = current.next;
-        }
-
-        if (current == null) return; // not found
-
-        int memoryFreed = current.seminar.toString().length();
-        memManager.deallocate(memoryFreed, memoryFreed);
-        prev.next = current.next;  // unlink the node
+    public void printHash() {
+        System.out.println("Hashtable: ");
+        hash.print();
     }
-
-    public Seminar search(int id) {
-        Node current = head;
-        while (current != null) {
-            if (current.seminar.id == id) {
-                return current.seminar;
-            }
-            current = current.next;
-        }
-        return null;  // not found
-    }
-
-    public void print() {
-        Node current = head;
-        while (current != null) {
-            System.out.println(current.seminar.toString());
-            current = current.next;
-        }
+    
+    public void printBlock() {
+        System.out.println("Freeblock List: ");
+        memManager.print();
     }
 }
-
