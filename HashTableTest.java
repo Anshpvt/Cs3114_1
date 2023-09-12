8eff56f4a1a600f4831bcf60e7b65e880a95ea59
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-12 19:32:33 -0400

    Update HashTableTest.java

--- HashTableTest.java
+++ HashTableTest.java
@@ -1,65 +1,60 @@
 import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
 
 public class HashTableTest {
 
     private HashTable hashTable;
 
     public void setUp() {
-        hashTable = new HashTable();
+        hashTable = new HashTable(10); // Initializing with capacity 10 for the test
     }
 
 
     public void testInsert() {
-        // Test insertion of a new record
-        assertTrue(hashTable.insert(1, "title1", "10:00", "2 hours", "1.2",
-            "2.3", "50", "list1", "description1"));
+        Record r1 = new Record(1, null); // Using null for the Seminar value just for testing
+        assertTrue(hashTable.insert(1, r1));
 
-        // Test that inserting a duplicate ID fails
-        assertFalse(hashTable.insert(1, "title2", "11:00", "3 hours", "2.3",
-            "3.4", "60", "list2", "description2"));
+        // Attempting to insert a duplicate key should return false
+        assertFalse(hashTable.insert(1, r1));
+    }
+
+    public void testSearch() {
+        Record r1 = new Record(2, null);
+        hashTable.insert(2, r1);
+
+        // Searching for a record that exists should return that record
+        assertEquals(r1, hashTable.search(2));
+
+        // Searching for a record that doesn't exist should return null
+        assertNull(hashTable.search(100));
     }
 
     public void testDelete() {
-        // Set up for delete
-        hashTable.insert(2, "title2", "11:00", "3 hours", "2.3", "3.4", "60",
-            "list2", "description2");
+        Record r2 = new Record(3, null);
+        hashTable.insert(3, r2);
 
-        // Test that deleting an existing record works
-        assertTrue(hashTable.delete(2));
+        // Deleting an existing record should return true
+        assertTrue(hashTable.delete(3));
 
-        // Test that deleting a non-existent record fails
-        assertFalse(hashTable.delete(2));
+        // Once deleted, searching for the record should return null
+        assertNull(hashTable.search(3));
+
+        // Deleting a record that doesn't exist should return false
+        assertFalse(hashTable.delete(100));
     }
 
-
-    public void testSearch() {
-        // Set up for search
-        hashTable.insert(3, "title3", "12:00", "4 hours", "3.4", "4.5", "70",
-            "list3", "description3");
-
-        // Test that searching for an existing record returns the correct record
-        String expected =
-            "ID: 3, Title: title3, Time: 12:00, Length: 4 hours, X: 3.4, Y: 4.5, Cost: 70, List: list3, Description: description3";
-        assertEquals(expected, hashTable.search(3));
-
-        // Test that searching for a non-existent record returns null
-        assertNull(hashTable.search(4));
+    public void testResize() {
+        for (int i = 0; i < 11; i++) {
+            hashTable.insert(i, new Record(i, null));
+        }
+        // After inserting 11 records, the hash table should resize (since it would exceed a 0.5 load factor)
+        assertEquals(20, hashTable.getCapacity());
+    }
+    
+    public void testGetCapacity() {
+        // Initial capacity should be 10 as set in setUp
+        assertEquals(10, hashTable.getCapacity());
     }
 
-
-    public void testPrint() {
-        // Set up for print
-        hashTable.insert(4, "title4", "01:00", "5 hours", "4.5", "5.6", "80",
-            "list4", "description4");
-        hashTable.insert(5, "title5", "02:00", "6 hours", "5.6", "6.7", "90",
-            "list5", "description5");
-
-        String expected =
-            "ID: 4, Title: title4, Time: 01:00, Length: 5 hours, X: 4.5, Y: 5.6, Cost: 80, List: list4, Description: description4\n"
-                + "ID: 5, Title: title5, Time: 02:00, Length: 6 hours, X: 5.6, Y: 6.7, Cost: 90, List: list5, Description: description5\n";
-
-        assertEquals(expected, hashTable.print());
-    }
+    // Note: For the print method, it would be better to check the output by redirecting the console output
+    // and comparing, but here it's not covered. Testing such methods typically requires different testing strategies.
 }
