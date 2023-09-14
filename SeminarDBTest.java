ba78b7eb5cf8786275f3642e9e9783ba5c9317e5
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-14 16:29:43 -0400

    Update SeminarDBTest.java

--- SeminarDBTest.java
+++ SeminarDBTest.java
@@ -1,8 +1,9 @@
 import static org.junit.Assert.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import student.TestCase;
 
-public class SeminarDBTest {
+public class SeminarDBTest extends TestCase {
 
     private SeminarDB db;
 
@@ -10,10 +11,11 @@ public class SeminarDBTest {
         db = new SeminarDB(1000, 10); // Initialize with arbitrary values
     }
 
+    // Test: Insertion of records
     public void testInsert() throws Exception {
         String[] keywords1 = {"AI", "ML"};
         Record r1 = new Record(1, new Seminar(1, "Intro to AI", "2023-10-01", 60, (short)100, (short)50, 200, keywords1, "An introduction to AI"));
-
+        
         // Capture the System.out output
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
@@ -26,6 +28,7 @@ public class SeminarDBTest {
         assertTrue(outContent.toString().contains("Insert FAILED - There is already a record with ID 1"));
     }
 
+    // Test: Deletion of records
     public void testDelete() throws Exception {
         String[] keywords2 = {"Data Science", "Stats"};
         Record r2 = new Record(2, new Seminar(2, "Data Analysis", "2023-11-10", 120, (short)150, (short)60, 250, keywords2, "An in-depth session on Data Analysis"));
@@ -43,6 +46,7 @@ public class SeminarDBTest {
         assertTrue(outContent.toString().contains("Delete FAILED -- There is no record with ID 100"));
     }
 
+    // Test: Searching for records
     public void testSearch() throws Exception {
         String[] keywords3 = {"Networking", "Security"};
         Record r3 = new Record(3, new Seminar(3, "Network Security", "2023-12-15", 90, (short)200, (short)70, 300, keywords3, "A workshop on Network Security"));
@@ -59,9 +63,28 @@ public class SeminarDBTest {
         assertTrue(outContent.toString().contains("Search FAILED -- There is no record with ID 100"));
     }
 
-    // Note: The printHash() and printBlock() methods would require a more complicated testing strategy
-    // to check their exact outputs. A simple way to test them would be to redirect the output to a stream
-    // and check if the output contains expected strings, similar to the above tests.
-    // However, this is not done here for the sake of brevity.
-}
+    // Test: Printing HashTable
+    public void testPrintHash() throws Exception {
+        // Insert a record for testing purposes
+        String[] keywords = {"Databases", "SQL"};
+        Record r = new Record(4, new Seminar(4, "SQL Basics", "2023-11-11", 60, (short)100, (short)60, 200, keywords, "Introduction to SQL"));
+        db.insert(r);
 
+        // Capture the System.out output
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        db.printHash();
+        assertTrue(outContent.toString().contains("Hashtable:"));
+    }
+
+    // Test: Printing Block
+    public void testPrintBlock() throws Exception {
+        // Capture the System.out output
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        db.printBlock();
+        assertTrue(outContent.toString().contains("Freeblock List:"));
+    }
+}
