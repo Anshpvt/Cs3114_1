83991b8980bc4bdc1a15b2c7b2849ca3aa7e3977
stepheny <112502475+dragofsewer@users.noreply.github.com>
2023-09-15 19:50:35 -0400

    Update HandleTest.java

--- HandleTest.java
+++ HandleTest.java
@@ -1,6 +1,7 @@
-import static org.junit.Assert.*;
 
-public class HandleTest {
+import student.TestCase;
+
+public class HandleTest extends TestCase{
 
     private Handle handle;
 
@@ -22,5 +23,31 @@ public class HandleTest {
     public void testGetLength() {
         // Testing getLength() method
         assertEquals(100, handle.getLength());
+    }
+    
+    public void testGetEnd() {
+        Handle handle = new Handle(1, 5, 10);
+        int expectedEnd = 14; // 5 + 10 - 1
+        assertEquals(expectedEnd, handle.getEnd());
+    }
+
+    public void testToString() {
+        Handle handle = new Handle(1, 5, 10);
+        String expectedString = "5 : 15";
+        assertEquals(expectedString, handle.toString());
+    }
+
+
+    public void testGetEndWithZeroLength() {
+        Handle handle = new Handle(1, 5, 0);
+        int expectedEnd = 4;
+        assertEquals(expectedEnd, handle.getEnd());
+    }
+
+
+    public void testToStringWithZeroLength() {
+        Handle handle = new Handle(1, 5, 0);
+        String expectedString = "5 : 5";
+        assertEquals(expectedString, handle.toString());
     }
 }
