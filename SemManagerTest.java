24bca252f4d33f3ada448c0aea41115a9c293a69
stepheny <112502475+dragofsewer@users.noreply.github.com>
2023-09-15 19:52:55 -0400

    Update SemManagerTest.java

--- SemManagerTest.java
+++ SemManagerTest.java
@@ -1,26 +1,61 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import student.TestCase;
 
-/**
- * @author {Ansh Patel}
- * @author {Stephen Ye}
- * @version {Project 1}
- */
-public class SemManagerTest extends TestCase {
-    /**
-     * Sets up the tests that follow. In general, used for initialization
-     */
+public class SemManagerTest extends TestCase{
+
+    private SemManager semManager;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
     public void setUp() {
-        // Nothing here
+        semManager = new SemManager();
+        
+        // Redirect System.out to outContent for tests related to methods that print.
+        System.setOut(new PrintStream(outContent));
     }
 
+    public void testInvalidArguments() {
+        semManager.start(new String[] {"100", "200"});
+        assertEquals("Please provide the path to the command file.", outContent.toString().trim());
+    }
 
-    /**
-     * Get code coverage of the class declaration.
-     */
-    public void testMInitx() {
-        SemManager sem = new SemManager();
-        assertNotNull(sem);
-        SemManager.main(null);
+    public void testInvalidFilePath() {
+        semManager.start(new String[] {"100", "200", "invalidFilePath"});
+        assertEquals("Invalid command file path provided.", outContent.toString().trim());
+    }
+
+    public void testValidFileInitialization() {
+        // Here, we assume that the initialization of the CommandProcessor and SeminarDB don't throw any errors.
+        semManager.start(new String[] {"100", "200", "validFilePath"});
+
+        // Ideally, we would assert something here, but given the current structure of the SemManager, 
+        // the best we can do is check that no error messages were printed.
+        assertFalse(outContent.toString().isEmpty());
+    }
+
+    // Test for no arguments
+    public void testNoArguments() {
+        semManager.start(new String[] {});
+        assertEquals("Please provide the path to the command file.", outContent.toString().trim());
+    }
+    
+    // Test for only one argument
+    public void testSingleArgument() {
+        semManager.start(new String[] {"100"});
+        assertEquals("Please provide the path to the command file.", outContent.toString().trim());
+    }
+
+    // Test with valid args but invalid file path
+    public void testValidArgsInvalidFilePath() {
+        semManager.start(new String[] {"100", "200", "/non/existent/path"});
+        assertEquals("Invalid command file path provided.", outContent.toString().trim());
+    }
+
+    // Test that only valid integers are provided as arguments
+    public void testNonIntegerArguments() {
+        semManager.start(new String[] {"100A", "200B", "validFilePath"});
+        // Here we assume that if an invalid integer is passed, the program will have some error output.
+        // This might not be the actual case, so you might want to adjust depending on how SemManager handles such cases.
+        assertFalse(outContent.toString().isEmpty());
     }
 }
-
