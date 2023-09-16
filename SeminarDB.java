bfc71cfcfe218e3ec93111f252d6d29fa7c0b950
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-16 16:05:57 -0400

    Update SeminarDB.java

--- SeminarDB.java
+++ SeminarDB.java
@@ -9,91 +9,93 @@ public class SeminarDB {
         this.memManager = new MemManager(memorySize);
         this.hash = new HashTable(hashSize);
     }
-    
-    public HashTable getHash()
-    {
+
+
+    public HashTable getHash() {
         return hash;
     }
-    
-    public boolean isInserted(int key, Record rec)
-    {
+
+
+    public boolean isInserted(int key, Record rec) {
         return hash.insert(key, rec);
     }
-    
-    public void insert(Record rec)
-    {
+
+
+    public void insert(Record rec) {
         int start = hash.getCapacity();
         int key = rec.getKey();
-        
-        if (hash.isFull(key, rec))
-        {
+
+        if (hash.isFull(key, rec)) {
             hash.resize();
         }
-        if (!isInserted(key, rec))
-        {
-            System.out.println("Insert FAILED - There is already a record with ID " + key);
+        if (!isInserted(key, rec)) {
+            System.out.println(
+                "Insert FAILED - There is already a record with ID " + key);
         }
-        else
-        {
+        else {
             Seminar sem = rec.getSeminar();
             System.out.println("Successfully inserted record with ID " + key);
             System.out.println(sem.toString());
-            
+
             byte[] byteArr;
             try {
                 byteArr = sem.serialize();
                 System.out.println("Size: " + byteArr.length);
-                int memoryNeeded = byteArr.length;  // assuming an average of 10 characters per keyword
+                int memoryNeeded = byteArr.length; // assuming an average of 10
+                                                   // characters per keyword
                 Handle address = memManager.insert(byteArr, memoryNeeded);
-                    if (address == null) {  // we dont have enough memory
-                        
-                        System.out.println("Not enough memory to store seminar with ID " + key);
-                    }  
+                if (address == null) { // we dont have enough memory
+
+                    System.out.println(
+                        "Not enough memory to store seminar with ID " + key);
                 }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }          
-        } 
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public boolean isDeleted(int key)
-    {
+
+    public boolean isDeleted(int key) {
         return hash.delete(key);
     }
-    
-    public void delete(int key) {
-        
-        if (!isDeleted(key))
-        {
-            System.out.println("Delete FAILED -- There is no record with ID " + key);
-        }
-        else
-        {
-            System.out.println("Record with ID " + key + " successfully deleted from the database");
 
-        
+
+    public void delete(int key) {
+
+        if (!isDeleted(key)) {
+            System.out.println("Delete FAILED -- There is no record with ID "
+                + key);
         }
-        
+        else {
+            System.out.println("Record with ID " + key
+                + " successfully deleted from the database");
+
+        }
+
     }
+
 
     public void search(int key) {
         Record rec = hash.search(key);
-        if (rec == null || rec.isTombstone())
-        {
-            System.out.println("Search FAILED -- There is no record with ID " + key);
+        if (rec == null || rec.isTombstone()) {
+            System.out.println("Search FAILED -- There is no record with ID "
+                + key);
         }
-        else
-        {
+        else {
             System.out.println("Found record with ID " + key + ": ");
             System.out.println(rec.getSeminar().toString());
         }
     }
 
+
     public void printHash() {
         System.out.println("Hashtable: ");
         hash.print();
     }
-    
+
+
     public void printBlock() {
         memManager.printBlocks();
         memManager.printList();
