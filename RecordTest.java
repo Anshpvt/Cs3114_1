6086de4be7038b3464ba4dc7378edd6a595d4f5f
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-16 16:05:05 -0400

    Update RecordTest.java

--- RecordTest.java
+++ RecordTest.java
@@ -1,27 +1,31 @@
 import static org.junit.Assert.*;
 import student.TestCase;
 
-public class RecordTest extends TestCase{
+public class RecordTest extends TestCase {
 
     private Record record;
     private Seminar seminar;
 
     public void setUp() {
         // Setting up a sample seminar and record
-        seminar = new Seminar(1, "Test Seminar", "2023-09-13", 120, (short)50, (short)60, 100, 
-                              new String[] {"Java", "OOP"}, "A seminar about Java OOP.");
+        seminar = new Seminar(1, "Test Seminar", "2023-09-13", 120, (short)50,
+            (short)60, 100, new String[] { "Java", "OOP" },
+            "A seminar about Java OOP.");
         record = new Record(1, seminar);
     }
-    
+
+
     public void testGetKey() {
         // Testing getKey() method
         assertEquals(1, record.getKey());
     }
-    
+
+
     public void testGetSeminar() {
         // Testing getSeminar() method
         assertEquals(seminar, record.getSeminar());
     }
+
 
     public void testIsTombstone() {
         // Testing isTombstone() method for normal record
