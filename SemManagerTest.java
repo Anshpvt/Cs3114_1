d246ae083d31c4c739137e37fa8e9739846e68ef
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-16 16:05:39 -0400

    Update SemManagerTest.java

--- SemManagerTest.java
+++ SemManagerTest.java
@@ -4,94 +4,123 @@ import student.TestCase;
 import java.io.File;
 import java.io.IOException;
 
-
-public class SemManagerTest extends TestCase{
+public class SemManagerTest extends TestCase {
 
     private SemManager semManager;
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream outContent =
+        new ByteArrayOutputStream();
 
     public void setUp() {
         semManager = new SemManager();
-        
+
         System.setOut(new PrintStream(outContent));
     }
 
+
     public void testInvalidArguments() {
-        semManager.start(new String[] {"100", "200"});
-        assertEquals("Please provide the path to the command file.", outContent.toString().trim());
+        semManager.start(new String[] { "100", "200" });
+        assertEquals("Please provide the path to the command file.", outContent
+            .toString().trim());
     }
+
 
     public void testInvalidFilePath() {
-        semManager.start(new String[] {"100", "200", "invalidFilePath"});
-        assertEquals("Invalid command file path provided.", outContent.toString().trim());
+        semManager.start(new String[] { "100", "200", "invalidFilePath" });
+        assertEquals("Invalid command file path provided.", outContent
+            .toString().trim());
     }
 
-    public void testValidFileInitialization() {        semManager.start(new String[] {"100", "200", "validFilePath"});
+
+    public void testValidFileInitialization() {
+        semManager.start(new String[] { "100", "200", "validFilePath" });
         assertFalse(outContent.toString().isEmpty());
     }
+
 
     // Test for no arguments
     public void testNoArguments() {
         semManager.start(new String[] {});
-        assertEquals("Please provide the path to the command file.", outContent.toString().trim());
+        assertEquals("Please provide the path to the command file.", outContent
+            .toString().trim());
     }
-    
+
+
     // Test for only one argument
     public void testSingleArgument() {
-        semManager.start(new String[] {"100"});
-        assertEquals("Please provide the path to the command file.", outContent.toString().trim());
+        semManager.start(new String[] { "100" });
+        assertEquals("Please provide the path to the command file.", outContent
+            .toString().trim());
     }
+
 
     // Test with valid args but invalid file path
     public void testValidArgsInvalidFilePath() {
-        semManager.start(new String[] {"100", "200", "/non/existent/path"});
-        assertEquals("Invalid command file path provided.", outContent.toString().trim());
+        semManager.start(new String[] { "100", "200", "/non/existent/path" });
+        assertEquals("Invalid command file path provided.", outContent
+            .toString().trim());
     }
+
 
     // Test that only valid integers are provided as arguments
     public void testNonIntegerArguments() {
-        semManager.start(new String[] {"100A", "200B", "validFilePath"});
+        semManager.start(new String[] { "100A", "200B", "validFilePath" });
         assertFalse(outContent.toString().isEmpty());
     }
+
+
     public void testMainWithInvalidArguments() {
-        SemManager.main(new String[] {"100", "200"});
-        assertEquals("Please provide the path to the command file.", outContent.toString().trim());
+        SemManager.main(new String[] { "100", "200" });
+        assertEquals("Please provide the path to the command file.", outContent
+            .toString().trim());
     }
+
 
     public void testMainWithInvalidFilePath() {
-        SemManager.main(new String[] {"100", "200", "invalidFilePath"});
-        assertEquals("Invalid command file path provided.", outContent.toString().trim());
+        SemManager.main(new String[] { "100", "200", "invalidFilePath" });
+        assertEquals("Invalid command file path provided.", outContent
+            .toString().trim());
     }
 
+
     public void testMainValidFileInitialization() {
-        SemManager.main(new String[] {"100", "200", "validFilePath"});
-        // Assuming there's another specific output you expect upon valid initialization
+        SemManager.main(new String[] { "100", "200", "validFilePath" });
+        // Assuming there's another specific output you expect upon valid
+        // initialization
         // Replace with the appropriate assertion
         assertFalse(outContent.toString().isEmpty());
     }
+
 
     public void testMainNoArguments() {
         SemManager.main(new String[] {});
-        assertEquals("Please provide the path to the command file.", outContent.toString().trim());
+        assertEquals("Please provide the path to the command file.", outContent
+            .toString().trim());
     }
-    
+
+
     public void testMainSingleArgument() {
-        SemManager.main(new String[] {"100"});
-        assertEquals("Please provide the path to the command file.", outContent.toString().trim());
+        SemManager.main(new String[] { "100" });
+        assertEquals("Please provide the path to the command file.", outContent
+            .toString().trim());
     }
+
 
     public void testMainValidArgsInvalidFilePath() {
-        SemManager.main(new String[] {"100", "200", "/non/existent/path"});
-        assertEquals("Invalid command file path provided.", outContent.toString().trim());
+        SemManager.main(new String[] { "100", "200", "/non/existent/path" });
+        assertEquals("Invalid command file path provided.", outContent
+            .toString().trim());
     }
 
+
     public void testMainNonIntegerArguments() {
-        SemManager.main(new String[] {"100A", "200B", "validFilePath"});
-        // Assuming there's another specific output you expect for non-integer arguments
+        SemManager.main(new String[] { "100A", "200B", "validFilePath" });
+        // Assuming there's another specific output you expect for non-integer
+        // arguments
         // Replace with the appropriate assertion
         assertFalse(outContent.toString().isEmpty());
     }
-    
+
+
     public void testValidFilePath() {
         File tempFile = null;
         try {
@@ -99,15 +128,20 @@ public class SemManagerTest extends TestCase{
             tempFile = File.createTempFile("testValidFilePath", ".txt");
             // Ensure the file will be deleted on JVM exit
             tempFile.deleteOnExit();
-            
-            semManager.start(new String[] {"100", "200", tempFile.getAbsolutePath()});
-            
-            // Since the file is valid, the output shouldn't contain the error message.
-            assertFalse("Invalid command file path provided.".equals(outContent.toString().trim()));
-            
-        } catch (IOException e) {
+
+            semManager.start(new String[] { "100", "200", tempFile
+                .getAbsolutePath() });
+
+            // Since the file is valid, the output shouldn't contain the error
+            // message.
+            assertFalse("Invalid command file path provided.".equals(outContent
+                .toString().trim()));
+
+        }
+        catch (IOException e) {
             fail("Failed to create a temporary file.");
-        } finally {
+        }
+        finally {
             if (tempFile != null && tempFile.exists()) {
                 tempFile.delete();
             }
