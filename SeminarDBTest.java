2a15e94f277260ac8e3270b5e83f8ee7bcbd87cb
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-15 23:43:55 -0400

    Update SeminarDBTest.java

--- SeminarDBTest.java
+++ SeminarDBTest.java
@@ -8,7 +8,7 @@ public class SeminarDBTest extends TestCase {
     private SeminarDB db;
 
     public void setUp() {
-        db = new SeminarDB(1000, 10); // Initialize with arbitrary values
+        db = new SeminarDB(1000, 10); 
     }
 
     // Test: Insertion of records
@@ -16,14 +16,11 @@ public class SeminarDBTest extends TestCase {
         String[] keywords1 = {"AI", "ML"};
         Record r1 = new Record(1, new Seminar(1, "Intro to AI", "2023-10-01", 60, (short)100, (short)50, 200, keywords1, "An introduction to AI"));
         
-        // Capture the System.out output
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
         db.insert(r1);
         assertTrue(outContent.toString().contains("Successfully inserted record with ID 1"));
-
-        // Attempting to insert a duplicate should produce an error message
         db.insert(r1);
         assertTrue(outContent.toString().contains("Insert FAILED - There is already a record with ID 1"));
     }
@@ -34,19 +31,18 @@ public class SeminarDBTest extends TestCase {
         Record r2 = new Record(2, new Seminar(2, "Data Analysis", "2023-11-10", 120, (short)150, (short)60, 250, keywords2, "An in-depth session on Data Analysis"));
         db.insert(r2);
 
-        // Capture the System.out output
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
         db.delete(2);
         assertTrue(outContent.toString().contains("Record with ID 2 successfully deleted from the database"));
 
-        // Trying to delete a non-existent record should produce an error message
         db.delete(100);
         assertTrue(outContent.toString().contains("Delete FAILED -- There is no record with ID 100"));
     }
 
     // Test: Searching for records
+ // Test: Searching for records
     public void testSearch() throws Exception {
         String[] keywords3 = {"Networking", "Security"};
         Record r3 = new Record(3, new Seminar(3, "Network Security", "2023-12-15", 90, (short)200, (short)70, 300, keywords3, "A workshop on Network Security"));
@@ -56,12 +52,28 @@ public class SeminarDBTest extends TestCase {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
+        // Test where rec is not null, and rec.isTombstone() returns false
         db.search(3);
         assertTrue(outContent.toString().contains("Found record with ID 3"));
+        outContent.reset();  // Clear the current content for the next test
 
+        // Test where rec is null
         db.search(100);
         assertTrue(outContent.toString().contains("Search FAILED -- There is no record with ID 100"));
+        outContent.reset();
+
+        // For this test, we need to insert a record, delete it (making it a tombstone), and then search for it.
+        // Test where rec is not null, but rec.isTombstone() returns true
+        String[] keywords4 = {"Cloud", "AWS"};
+        Record r4 = new Record(4, new Seminar(4, "Cloud Computing", "2023-11-20", 90, (short)220, (short)80, 320, keywords4, "A workshop on Cloud Computing"));
+        db.insert(r4);
+        db.delete(4); 
+        db.search(4);
+        assertTrue(outContent.toString().contains("Search FAILED -- There is no record with ID 4"));
     }
+
+    
+    
 
     // Test: Printing HashTable
     public void testPrintHash() throws Exception {
@@ -99,5 +111,45 @@ public class SeminarDBTest extends TestCase {
 
     }
     
-    
+    public void testHashTableFullAndResize() {
+        // 1. Full HashTable Test
+        SeminarDB testDB1 = new SeminarDB(1000, 1);
+        String[] keywords1 = {"AI", "ML"};
+        Record r1 = new Record(1, new Seminar(1, "Intro to AI", "2023-10-01", 60, (short)100, (short)50, 200, keywords1, "An introduction to AI"));
+        Record r2 = new Record(2, new Seminar(2, "Advanced AI", "2023-11-01", 70, (short)110, (short)60, 210, keywords1, "Advanced AI topics"));
+        testDB1.insert(r1);
+        testDB1.insert(r2); 
+        assertTrue(testDB1.getHash().getCapacity() > 1); 
+
+        // 2. Not Full HashTable Test
+        SeminarDB testDB2 = new SeminarDB(1000, 10);
+        testDB2.insert(r1);
+        assertEquals(10, testDB2.getHash().getCapacity()); 
+    }
+
+ // Test where there is not enough memory
+    public void testMemoryInsufficient() throws Exception {
+        SeminarDB testDB1 = new SeminarDB(10, 10); 
+        String[] keywords = {"AI", "ML"};
+        Record r1 = new Record(1, new Seminar(1, "Intro to AI", "2023-10-01", 60, (short)100, (short)50, 200, keywords, "An introduction to AI"));
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        testDB1.insert(r1);
+        assertTrue(outContent.toString().contains("Not enough memory to store seminar with ID 1"));
+    }
+
+    // Test where there is enough memory
+    public void testMemorySufficient() throws Exception {
+        SeminarDB testDB2 = new SeminarDB(1000, 10);
+        String[] keywords = {"AI", "ML"};
+        Record r2 = new Record(1, new Seminar(1, "Intro to AI", "2023-10-01", 60, (short)100, (short)50, 200, keywords, "An introduction to AI"));
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        testDB2.insert(r2);
+        assertFalse(outContent.toString().contains("Not enough memory to store seminar with ID 1"));
+    }
 }
