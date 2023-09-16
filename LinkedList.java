f22307837cb9c3abd01c3a02888977bc3d6391fb
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-16 00:16:40 -0400

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,83 +1,83 @@
-public class LinkedList {
-    private Node head;
-    private Node tail;
-
-    public LinkedList() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    // Insert node to the end of the list
-    public void insert(Handle handle) {
-        Node newNode = new Node(handle);
-        if (isEmpty()) {
-            head = tail = newNode;
-        } else {
-            tail.next = newNode;
-            tail = newNode;
+    public class LinkedList {
+        private Node head;
+        private Node tail;
+    
+        public LinkedList() {
+            this.head = null;
+            this.tail = null;
         }
-    }
-
-    // Remove node with the given handle
-    public void remove(Handle handle) {
-        if (isEmpty()) return;
-
-        if (head.handle.equals(handle)) {
-            head = head.next;
-            return;
+    
+        // Insert node to the end of the list
+        public void insert(Handle handle) {
+            Node newNode = new Node(handle);
+            if (isEmpty()) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
         }
-
-        Node prev = null;
-        Node current = head;
-        while (current != null && !current.handle.equals(handle)) {
-            prev = current;
-            current = current.next;
+    
+        // Remove node with the given handle
+        public void remove(Handle handle) {
+            if (isEmpty()) return;
+    
+            if (head.handle.equals(handle)) {
+                head = head.next;
+                return;
+            }
+    
+            Node prev = null;
+            Node current = head;
+            while (current != null && !current.handle.equals(handle)) {
+                prev = current;
+                current = current.next;
+            }
+    
+            if (current != null) {
+                prev.next = current.next;
+                if (current.next == null) {  // adjusting tail
+                    tail = prev;
+                }
+            }
         }
-
-        if (current != null) {
-            prev.next = current.next;
-            if (current.next == null) {  // adjusting tail
-                tail = prev;
+    
+        // Check if the list contains the handle
+        public boolean contains(Handle handle) {
+            Node current = head;
+            while (current != null) {
+                if (current.handle.equals(handle)) return true;
+                current = current.next;
+            }
+            return false;
+        }
+    
+        // Get the head of the list
+        public Node getHead() {
+            return head;
+        }
+    
+        // Check if the list is empty
+        public boolean isEmpty() {
+            return head == null;
+        }
+    
+        // Print the list
+        public void printList() {
+            Node current = head;
+            while (current != null) {
+                System.out.println(current.handle);
+                current = current.next;
             }
         }
     }
-
-    // Check if the list contains the handle
-    public boolean contains(Handle handle) {
-        Node current = head;
-        while (current != null) {
-            if (current.handle.equals(handle)) return true;
-            current = current.next;
-        }
-        return false;
-    }
-
-    // Get the head of the list
-    public Node getHead() {
-        return head;
-    }
-
-    // Check if the list is empty
-    public boolean isEmpty() {
-        return head == null;
-    }
-
-    // Print the list
-    public void printList() {
-        Node current = head;
-        while (current != null) {
-            System.out.println(current.handle);
-            current = current.next;
+    
+    class Node {
+        Handle handle;
+        Node next;
+    
+        public Node(Handle handle) {
+            this.handle = handle;
+            this.next = null;
         }
     }
-}
-
-class Node {
-    Handle handle;
-    Node next;
-
-    public Node(Handle handle) {
-        this.handle = handle;
-        this.next = null;
-    }
-}
