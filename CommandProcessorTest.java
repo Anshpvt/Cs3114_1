a17fe96543957bcd2fa40915ad63fefed604f285
stepheny <112502475+dragofsewer@users.noreply.github.com>
2023-09-16 22:45:51 -0400

    Update CommandProcessorTest.java

--- CommandProcessorTest.java
+++ CommandProcessorTest.java
@@ -42,22 +42,26 @@ public class CommandProcessorTest extends TestCase {
     private String printBlockStr;
     private String unknownCommandStr;
 
-    /*
+    /**
      * sets up for tests
      */
     public void setUp() {
         insertStr = "Successfully inserted record with ID 1\n"
             + "ID: 1, Title: Overview of HCI Research at VT\n"
             + "Date: 0610051600, Length: 90, X: 10, Y: 10, Cost: 45\n"
-            + "Description: This seminar will present an overview of HCI research at VT\n"
+            + "Description: This seminar will present an overview "
+            + "of HCI research at VT\n"
             + "Keywords: HCI, Computer_Science, VT, Virginia_Tech\n"
             + "Size: 173";
         searchStr = "Found record with ID 3:\n"
             + "ID: 3, Title: Computing Systems Research at VT\n"
-            + "Date: 0701250830, Length: 30, X: 30, Y: 10, Cost: 17\n"
+            + "Date: 0701250830, Length: 30, X: 30, Y: 10, Cost: "
+            + "17\n"
             + "Description: Seminar about the      Computing systems research at      VT\n"
-            + "Keywords: high_performance_computing, grids, VT, computer, science";
-        printHashStr = "Hashtable:\n" + "1: 1\n" + "2: 2\n" + "3: 3\n"
+            + "Keywords: high_performance_computing, grids, VT, computer, "
+            + "science";
+        printHashStr = "Hashtable:\n" + "1: 1\n" + "2: "
+        		+ "2\n" + "3: 3\n"
             + "5: 10\n" + "total records: 4";
         printBlockStr = "Freeblock List:\n" + "256: 256";
         unknownCommandStr = "unreconized input look\n";
